609060140fd43c7e32ccbf23e618a19668306863
Fellipe Lima <140414505+francafellipe@users.noreply.github.com>
2024-06-28 12:30:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,28 +21,17 @@ async function cadastrarNovoProduto() {
 
         // Criar um novo elemento com as mesmas características da div
         const novoElemento = document.createElement('div');
-        novoElemento.className = 'container text-center';
-        novoElemento.id = 'novo-elemento'; // Defina um ID único para o novo elemento
-
         // Adicionar conteúdo ao novo elemento (pode ser semelhante ao conteúdo da div original)
         novoElemento.innerHTML = `
-            <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-                <div class="col">
-                    <div class="p-3">
-                        <div class="card" style="width: 18rem;">
-                            <img src="${data.image}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${data.title}</h5>
-                                <p class="card-text">${data.description}.</p>
-                                <a href="#" class="btn btn-primary">R$ ${data.price}</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="card" style="width: 18rem; margin:10px;">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.title}</h5>
+                <p class="card-text">${data.description}.</p>
+                <a href="#" class="btn btn-primary">R$ ${data.price}</a>
             </div>
+        </div>
         `;
-
-        // Adicionar o novo elemento à main
         mainProdutos.appendChild(novoElemento);
     } catch (error) {
         console.error('Erro ao cadastrar o novo produto:', error);
